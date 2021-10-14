162adff33375668483fb7b0369b7debb19087e9d
alex3 <alexprocards@mail.ru>
2021-10-15 04:52:49 +0500

    4.2. Тернарное сравнение [#189 #215964]

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -17,7 +17,7 @@ public class MaxTest {
     public void whenMax10To5Then10() {
         int left = 10;
         int right = 5;
-        int result = Max.max(left, right);
+        int result = Max.max(right, left);
         int expected = 10;
         Assert.assertEquals(result, expected);
     }
