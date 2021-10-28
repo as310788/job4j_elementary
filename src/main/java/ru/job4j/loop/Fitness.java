025f5dd55bff7f110b733a5444bd19cbf80c2cb1
alex3 <alexprocards@mail.ru>
2021-10-28 14:29:28 +0500

    5.4. Протеиновая диета [#156313]

---
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nik) {
+        int month = 0;
+        while (ivan <= nik) {
+            month++;
+            ivan *= 3;
+            nik *= 2;
+        }
+        return month;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calc(10, 80));
+    }
+}
+
+
